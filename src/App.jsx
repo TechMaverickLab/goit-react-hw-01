e3daf946805dc5178dc28265dc5e30e9b076c158
TechMaverickLab <TechMaverickLab@gmail.com>
2024-01-20 21:36:08 -0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@
 import Profile from './components/Profile/Profile';
 import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import userData from './data/userData.json';
-import friends from './data/friends.json';
-import transactions from './data/transactions.json';
+import userData from './userData.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
 
 
 const App = () => {
